dfa3965dd553e6707df100f299696582f7d94876
Berkaytlli <hbtelli7@gmail.com>
2022-04-13 21:39:30 +0300

    Shopping Site Project

---
+++ ShopSiteApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopSiteApp.Data;
+
+[assembly: HostingStartup(typeof(ShopSiteApp.Areas.Identity.IdentityHostingStartup))]
+namespace ShopSiteApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
